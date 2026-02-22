063f5018af06e5486a2133cc36a4e42f7b18f2a3
Dama5323 <your-deenyashke@gmail.com>
2026-02-22 22:37:49 +0300

    test(todo): implement TodoList with Jest tests

---
+++ react-todo/src/__tests__/TodoList.test.js
@@ -0,0 +1,47 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import TodoList from "../components/TodoList";
+
+test("renders initial todos", () => {
+  render(<TodoList />);
+
+  expect(screen.getByText("Learn React")).toBeInTheDocument();
+  expect(screen.getByText("Write Tests")).toBeInTheDocument();
+});
+
+test("adds a new todo", () => {
+  render(<TodoList />);
+
+  fireEvent.change(
+    screen.getByPlaceholderText("Add todo"),
+    { target: { value: "New Task" } }
+  );
+
+  fireEvent.click(screen.getByText("Add"));
+
+  expect(screen.getByText("New Task"))
+    .toBeInTheDocument();
+});
+
+test("toggles todo completion", () => {
+  render(<TodoList />);
+
+  const todo = screen.getByText("Learn React");
+
+  fireEvent.click(todo);
+
+  expect(todo).toHaveStyle("text-decoration: line-through");
+});
+
+test("deletes todo", () => {
+  render(<TodoList />);
+
+  const deleteButtons =
+    screen.getAllByText("Delete");
+
+  fireEvent.click(deleteButtons[0]);
+
+  expect(
+    screen.queryByText("Learn React")
+  ).not.toBeInTheDocument();
+});
